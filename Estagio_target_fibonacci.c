baa572d682c3bef02aab2e09cae9e4477fed4189
VitorPortinari <98242331+VitorPortinari@users.noreply.github.com>
2024-09-21 20:05:04 -0300

    Update Estagio_target_fibonacci.c

--- Estagio_target_fibonacci.c
+++ Estagio_target_fibonacci.c
@@ -1,32 +1,28 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
     int x = 0, z = 1, prox_num_fibo = 0, valor_digitado = 0;
-    
-    printf("Digite um valor:\n");
-    scanf("%d",&valor_digitado);
-    
+
+    printf("Digite um valor: ");
+    scanf("%d", &valor_digitado);
+
     if (valor_digitado == x || valor_digitado == z) {
-        printf("O numero pertence a sequencia"); 
+        printf("O numero pertence a sequencia\n");
         return 0;
     }
-    //Verifica se o valor digitado pertence a sequencia
+
+    // Verifica se o valor digitado pertence à sequência
     while (prox_num_fibo < valor_digitado) {
         prox_num_fibo = x + z;
         x = z;
         z = prox_num_fibo;
 
         if (prox_num_fibo == valor_digitado) {
-            printf("O numero pertence a sequencia"); 
-            return 0;
-        }
-        else{
-            printf("O numero não pertence a sequencia");
+            printf("O numero pertence a sequencia\n");
             return 0;
         }
     }
-    
+
+    printf("O numero não pertence a sequencia\n");
     return 0;
 }
-
